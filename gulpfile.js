ad1a948e589e3bb1c48835b8fdc58e28faa80276
Aaron Edwards <aaron@uglyrobot.com>
2020-07-21 15:30:53 -0500

    Update gulp tasks and add zip task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
-var gulp = require('gulp');
-var wpPot = require('gulp-wp-pot');
+const gulp = require('gulp');
+const wpPot = require('gulp-wp-pot');
+const zip = require('gulp-zip');
 
-gulp.task('default', function () {
+gulp.task('pot', function () {
   return gulp.src(['**/*.php', '!node_modules/**', '!vendor/**'])
     .pipe(wpPot({
       domain: 'iup',
@@ -9,3 +10,21 @@ gulp.task('default', function () {
     }))
     .pipe(gulp.dest('infinite-uploads.pot'));
 });
+
+gulp.task('zip', gulp.series('pot', function () {
+  return gulp.src([
+    './**/*',
+    '!node_modules/**',
+    '!bin/**',
+    '!tests/**',
+    '!./gulpfile.js',
+    '!./package.json',
+    '!./package-lock.json',
+    '!./phpunit.xml.dist',
+    '!./.*'
+  ])
+    .pipe(zip('infinite-uploads.zip'))
+    .pipe(gulp.dest('./../'));
+}));
+
+gulp.task('default', gulp.series('pot'));
