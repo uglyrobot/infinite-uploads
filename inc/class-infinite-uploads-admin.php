e02daeef3a7b4eaf550ba23315c36fd22ccc696d
Aaron Edwards <aaron@uglyrobot.com>
2020-12-01 22:28:23 -0600

    Add placeholder for bottom banner on plugin pages

--- inc/class-infinite-uploads-admin.php
+++ inc/class-infinite-uploads-admin.php
@@ -581,6 +581,26 @@ class Infinite_Uploads_Admin {
 			}
 			?>
 		</div>
+		<div class="container mt-5">
+			<div class="row">
+				<div class="col-sm text-center text-muted">
+					<strong><?php _e( "The Cloud by Infinite Uploads", 'infinite-uploads' ); ?></strong>
+				</div>
+			</div>
+			<div class="row mt-3">
+				<div class="col-sm text-center text-muted">
+					<a href="#" class="text-muted"><?php _e( "Support", 'infinite-uploads' ); ?></a> |
+					<a href="#" class="text-muted"><?php _e( "Terms of Service", 'infinite-uploads' ); ?></a> |
+					<a href="#" class="text-muted"><?php _e( "Privacy Policy", 'infinite-uploads' ); ?></a>
+				</div>
+			</div>
+			<div class="row mt-3">
+				<div class="col-sm text-center text-muted">
+					<a href="#" class="text-muted" data-toggle="tooltip" title="<?php esc_attr_e( 'Twitter', 'infinite-uploads' ); ?>"><span class="dashicons dashicons-twitter"></span></a>
+					<a href="#" class="text-muted" data-toggle="tooltip" title="<?php esc_attr_e( 'Facebook', 'infinite-uploads' ); ?>"><span class="dashicons dashicons-facebook-alt"></span></a>
+				</div>
+			</div>
+		</div>
 		<?php
 	}
 }
