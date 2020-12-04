cae2641315b30fbbb974a40fa35772957892f818
Aaron Edwards <aaron@uglyrobot.com>
2020-12-03 20:39:44 -0600

    Add uninstaller

---
+++ uninstall.php
@@ -0,0 +1,24 @@
+<?php
+// if uninstall.php is not called by WordPress, die
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	die;
+}
+
+//delete options except site_id
+if ( is_multisite() ) {
+	delete_site_option( 'iup_installed' );
+	delete_site_option( 'iup_files_scanned' );
+	delete_site_option( 'iup_enabled' );
+	delete_site_option( 'iup_apitoken' );
+	delete_site_option( 'iup_api_data' );
+} else {
+	delete_option( 'iup_installed' );
+	delete_option( 'iup_files_scanned' );
+	delete_option( 'iup_enabled' );
+	delete_option( 'iup_apitoken' );
+	delete_option( 'iup_api_data' );
+}
+
+// drop a custom database table
+global $wpdb;
+$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->base_prefix}infinite_uploads_files" );
