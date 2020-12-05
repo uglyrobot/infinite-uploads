f03366296e3da4868c54e5be5df5c420c42db7b5
Aaron Edwards <aaron@uglyrobot.com>
2020-12-05 17:29:53 -0600

    Clean up settings

--- inc/templates/settings.php
+++ inc/templates/settings.php
@@ -20,7 +20,7 @@
 			<div class="col">
 				<div class="row">
 					<div class="col"><?php _e( 'Used / Available', 'infinite-uploads' ); ?> <span class="dashicons dashicons-info text-muted" data-toggle="tooltip" title="<?php esc_attr_e( 'Recalculated every 24 hours', 'infinite-uploads' ); ?>"></span></div>
-					<div class="col text-right"><?php _e( 'Need more?', 'infinite-uploads' ); ?> <a href="#" class="text-warning"><?php _e( 'Switch to a new plan.', 'infinite-uploads' ); ?></a></div>
+					<div class="col text-right"><?php _e( 'Need more?', 'infinite-uploads' ); ?> <a href="<?php echo $this->api_url( '/account/' ); ?>" class="text-warning"><?php _e( 'Switch to a new plan.', 'infinite-uploads' ); ?></a></div>
 				</div>
 				<div class="row">
 					<div class="col badge badge-pill badge-light text-left p-3">
@@ -47,12 +47,14 @@
 		<div class="row justify-content-center mb-5">
 			<div class="col">
 				<h5><?php _e( 'CDN URL', 'infinite-uploads' ); ?></h5>
-				<p class="lead"><?php _e( 'Your uploads are served from this CDN url via 45 edge locations around the world.', 'infinite-uploads' ); ?></p>
+				<p class="lead"><?php _e( 'Your uploads are served from this CDN url via 45+ edge locations around the world.', 'infinite-uploads' ); ?></p>
 			</div>
 			<div class="col">
 				<div class="row">
 					<div class="col"><?php _e( 'Current CDN URL', 'infinite-uploads' ); ?></div>
-					<div class="col text-right"><?php _e( 'Use your own domain!', 'infinite-uploads' ); ?> <a href="#" class="text-warning"><?php _e( 'Upgrade to a business plan.', 'infinite-uploads' ); ?></a></div>
+					<?php if ( $api_data->site->cname == $api_data->site->cdn_url ) { ?>
+					<div class="col text-right"><a href="<?php echo $this->api_url( '/account/' ); ?>" class="text-warning"><?php _e( 'Use your own custom domain!', 'infinite-uploads' ); ?></a></div>
+					<?php } ?>
 				</div>
 				<div class="row">
 					<div class="col badge badge-pill badge-light text-left p-3">
@@ -97,7 +99,7 @@
 		<div class="row justify-content-center">
 			<div class="col">
 				<h5><?php _e( 'Import & Disconnect', 'infinite-uploads' ); ?></h5>
-				<p class="lead"><?php printf( __( 'Download your media files and disconnect from our cloud. To cancel or manage your storage plan please visit <a href="%s">account management</a>.', 'infinite-uploads' ), $this->api_url( '/account/' ) ); ?></p>
+				<p class="lead"><?php printf( __( 'Download your media files and disconnect from our cloud. To cancel or manage your storage plan please visit <a href="%s" class="text-warning">account management</a>.', 'infinite-uploads' ), $this->api_url( '/account/' ) ); ?></p>
 			</div>
 			<div class="col mt-4">
 				<div class="row text-center mb-3">
